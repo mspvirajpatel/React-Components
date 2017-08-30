bf828f367e342452f28adcf410c7346cceb52c89
Viraj Patel <mspviraj@hotmail.com>
2017-08-30 17:23:23 +0530

    -RespnsiveImage Added

---
+++ component/ResponsiveImage/device.android.js
@@ -0,0 +1,16 @@
+var React = require('react');
+var { Dimensions } = require('react-native');
+
+var device = Dimensions.get('window');
+var scale;
+
+if (device.width <= 414) {
+    //Android smartphones
+    scale = device.width / 414;
+} else{
+    //Android tablets
+    scale = 1;
+}
+
+module.exports = { scale: scale };
+
